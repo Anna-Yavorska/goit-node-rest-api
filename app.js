9cd0bcda324f1f56c9197ddffda8a3955e79b8ba
Anna Yavorska <aniayavorsk@gmail.com>
2024-02-09 04:13:25 +0100

    add login.test

--- app.js
+++ app.js
@@ -27,15 +27,5 @@ app.use((err, req, res, next) => {
   res.status(status).json({ message });
 });
 
-mongoose
-  .connect(DB_HOST)
-  .then(() => console.log("Database connection successful"))
-  .then(() =>
-    app.listen(3000, () => {
-      console.log("Server is running. Use our API on port: 3000");
-    })
-  )
-  .catch((err) => {
-    console.error(err.message);
-    process.exit(1);
-  });
+module.exports = app;
+
